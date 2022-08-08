747047e7d8c4771470716792b44d0042f56a9583
Kelly Duran <kaduran16@gmail.com>
2022-08-08 19:59:47 -0400

    commit tree 7f1524da456e1fb8481c992fd90a43108dbd9c82

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(countdown_to_drop)
+  while countdown_to_drop > 0
+    puts "#{countdown_to_drop} SECONDS(S)!"
+    countdown_to_drop -= 1
+  end
+  'HAPPY NEW YEAR!'
+end
 
-def countdown
+def countdown_with_sleep(countdown_to_drop)
+  while countdown_to_drop > 0
+    puts "#{countdown_to_drop} SECOND(S)!"
+    sleep(1)
+    countdown_to_drop -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
